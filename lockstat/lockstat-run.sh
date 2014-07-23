5815d37e2b4469347848396ac3aa8747562dd98d
1ruge <hammel@lenovo>
2014-07-23 19:16:34 +0200

    opcontrol added to lockstat-run

--- lockstat/lockstat-run.sh
+++ lockstat/lockstat-run.sh
@@ -3,6 +3,10 @@
 #$1=test name
 #$2..$#= cmd
 
+vmlinux="/home/hammel/linux/vmlinux"
+procfiles="/proc/stat /proc/lock_stat /proc/diskstats "
+kernel_mod="/home/hammel/linux/"
+
 #get the location of the current script
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -34,10 +38,28 @@ fi
 
 echo "$cmd" > "$test_dir/cmd"
 
+#init lockstat
 echo "clearing lockstat"
 sudo su -m -c "echo 0 > /proc/lock_stat"
 
-procfiles="/proc/stat /proc/lock_stat /proc/diskstats "
+#init Oprofile
+if [ "$(which opcontrol 2>/dev/null)" = "" ]; then
+	echo "starting oprofile"
+	sudo opcontrol --reset
+	sudo opcontrol --deinit
+	sudo modprobe oprofile timer=1
+	sudo su -m -c "echo 0 > /proc/sys/kernel/nmi_watchdog"
+	sudo opcontrol --separate=cpu
+fi
+
+#if [ $oprof_mode = "separate" ]; then
+#else
+#	sudo opcontrol --separate=none
+#fi
+rm -rf "$test_dir/oprofile_data/"
+sudo opcontrol --start --vmlinux="$vmlinux" --session-dir="$test_dir/oprofile_data/"
+
+#sampling deamon for /proc files
 sample_dir="$test_dir/samples"
 mkdir -p "$sample_dir"
 rm -f "$sample_dir"/*
@@ -54,14 +76,24 @@ eval $cmd $i &> "$test_dir/user_output"
 end_t=`date +%H-%M-%S`
 ##############
 
+#deinit sampling deamon
 echo "killing sampling deamon..."
 sudo kill -SIGTERM $sampling_deamon_pid
 wait $sampling_deamon_pid
 
+#deinit oprofile
+if [ "$(which opcontrol 2>/dev/null)" = "" ]; then
+	sudo opcontrol --stop
+	sudo opcontrol --dump
+	sudo opcontrol --shutdown
+	opreport --session-dir="$test_dir/oprofile_data/" -p $kernel_mod -l > "$test_dir/oprof_results_separate"
+fi
+
 user=$USER
 group=$(id -gn)
-sudo chown $user:$group "$sample_dir"/*
-chmod 660 "$sample_dir"/*
+sudo chown -R $user:$group "$test_dir"
+find "$test_dir" -type d -exec chmod 755 {} \;
+find "$test_dir" -type f -exec chmod 644 {} \;
 
 sudo cat /proc/lock_stat > "$test_dir/lock_stat"
 
