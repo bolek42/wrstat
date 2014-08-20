6b5e304a944ef8ab05cc33324f08840bcf15c08d
1ruge <hammel@wr-ubuntu>
2014-08-20 21:48:00 +0200

    utils.py added

--- lockstat/lockstat-graph.py
+++ lockstat/lockstat-graph.py
@@ -1,33 +1,11 @@
 #!/usr/bin/python
 
-import os
 import sys
-import csv
-import operator
 import pickle
 
+from utils import *
+
 sample_rate = 1.0
-
-import imp
-
-def load_config( filename): 
-	#read samplerate from config
-	file = open( filename, "r")
-	raw = list( csv.reader( file, delimiter=' '))
-	rows = map( lambda row: filter(lambda s: s != '', row), raw)
-
-	config = {}
-	for row in rows:
-		config[ row[0]] = row[ 1:]
-
-	return config
-
-def load_modules( modnames):
-	modules = {}
-	for modname in modnames:
-		modules[ modname] =  imp.load_source( modname, "%s.py" % modname)
-
-	return modules
 
 if __name__ == "__main__":
 	if len( sys.argv) != 2:
@@ -36,7 +14,7 @@ if __name__ == "__main__":
 
 	#read samplerate from config
 	config = load_config( "%s/lockstat.config" % sys.argv[1])
-	sample_rate = float( config[ "samprate"][0])
+	sample_rate = float( config[ "samprate"])
 	modules = load_modules( config[ "modules"])
 
 	# load samples
---
+++ lockstat/utils.py
@@ -0,0 +1,24 @@
+import imp
+import csv
+
+def load_config( filename): 
+	#read samplerate from config
+	file = open( filename, "r")
+	raw = list( csv.reader( file, delimiter=' '))
+	rows = map( lambda row: filter(lambda s: s != '', row), raw)
+
+	config = {}
+	for row in rows:
+		if len( row) == 2:
+			config[ row[0]] = row[ 1:][0]
+		elif len( row) > 2:
+			config[ row[0]] = row[ 1:]
+
+	return config
+
+def load_modules( modnames):
+	modules = {}
+	for modname in modnames:
+		modules[ modname] =  imp.load_source( modname, "%s.py" % modname)
+
+	return modules
