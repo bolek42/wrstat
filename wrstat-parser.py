f4d84b9dc44e8e9ac0d3d0436abffdc9914e6c48
1ruge <hammel@wr-ubuntu>
2014-08-26 23:48:34 +0200

    lockstat renamed to wrstat; forgotten files

---
+++ wrstat-parser.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import os
+import sys
+import csv
+import pickle
+
+from utils import *
+
+intervall = 1.0
+
+## actual main ##
+if __name__ == "__main__":
+	if len( sys.argv) != 2:
+		print "usage: %s test_dir" % sys.argv[0]
+		exit(1)
+
+	sample_file = "%s/samples.pickle" % sys.argv[1]
+
+	#read config file
+	config = load_config( "%s/wrstat.config" % sys.argv[1])
+	intervall = float( config[ "intervall"])
+	modules = load_modules( config[ "modules"])
+
+	#calling modules for parsing
+	samples = {}
+	for modname, module in modules.iteritems():
+		print "%s: calling module %s" % ( __file__, modname)
+		data = module.parse( sys.argv[1])
+		samples[ modname] = data
+	
+	f = open( sample_file, "w")
+	pickle.dump( samples, f)
+	f.close()
