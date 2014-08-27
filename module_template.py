547c164e25bfb6e80cf319243a752cea1c98dc88
1ruge <hammel@wr-ubuntu>
2014-08-27 22:52:17 +0200

    added module template without comments

---
+++ module_template.py
@@ -0,0 +1,43 @@
+import os
+import os
+
+import graphing
+from utils import *
+
+#########################################
+#       Sampling methods                #
+#########################################
+
+def presampling( test_dir):
+	pass
+
+def sample( test_dir, t):
+	pass
+
+def postsampling( test_dir):
+	pass
+
+
+#########################################
+#       Parsing samples                 #
+#########################################
+
+def parse( test_dir):
+	#t = 0
+	#samples = []
+	#while os.path.isfile( "%s/samples/modulename_%d" % ( test_dir, t)):
+	#	sample = parse_sample( "%s/samples/module_%d" % ( test_dir, t)) #TODO Implement
+	#	samples.append( sample)
+
+	#	t+= 1
+
+	return None
+
+
+
+#########################################
+#       Plotting data                   #
+#########################################
+
+def plot( test_dir, data, intervall):
+	pass
