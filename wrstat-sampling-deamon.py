f368eb53f535c6685697a9ddad75580300d68d27
1ruge <hammel@wr-ubuntu>
2014-09-17 06:40:29 +0200

    samplig deamon create less threads

--- wrstat-sampling-deamon.py
+++ wrstat-sampling-deamon.py
@@ -2,76 +2,109 @@
 import sys
 import signal
 import threading
+import time
 import shutil
 import csv
 
 from utils import *
 
-modules = []
-t = 0
+modules = []            #list of callable modules
+sampling_threads = []   #list of sampling threads
+cv = None               #condition variable used for thread synchronisation
+t = 0                   #current sample
+run = True              #True if deamon is running
 test_dir = ""
-run = True
 _file = __file__
 
-def capture():
+
+#The actual thread taking samples for a single module
+def sample( module):
+    global cv
+    while run:
+        module.sample( test_dir, t)
+
+        cv.acquire()
+        cv.wait()
+        cv.release()
+
+
+#timing thread which will wake up all threads at the specified intervall
+def timer( intervall):
     global t
-    threads = []
-    for modname, module in modules.iteritems():
-        thread = threading.Thread( target=module.sample, args=( test_dir, t))
-        thread.start()
-        threads.append( thread)
-    t += 1
+    global cv
 
-    if run:
-        threading.Timer( intervall, capture).start()
+    while run:
+        #waking up all waiting samping threads
+        cv.acquire()
+        cv.notify_all()
+        cv.release()
 
-    for thread in threads:
-        thread.join()
+        time.sleep( intervall)
+        t += 1
 
-    #print "sample"
 
+#catch a signal ( e.g. SIGINT) and shuts down the deamon
 def signal_handler(signal, frame):
-    #print "captured signal " + str( signal)
+    global run
 
-    print "captured %d samples" % t
+    print "stopping all sampling threads..."
+    run = False
+
+    #wake up all waiting sampling threads for a clean exit
+    finished = False
+    while not finished:
+        finished = True
+        for t in sampling_threads:
+            if t.is_alive():
+                finished = False
+
+        cv.acquire()
+        cv.notify_all()
+        cv.release()
+
+    #deinitialize the modules
     print "running postsampling..."
     for modname, module in modules.iteritems():
         print "%s: deinitialize %s" % ( _file, modname)
         module.postsampling( test_dir)
 
-    #stopping sampling deamon
-    global run
-    run = False
-
-    #waiting for all threads to exit
+    #just to be safe, waiting for all threads to exit
     for thread in threading.enumerate():
         if thread != threading.current_thread():
             thread.join()
+
 
 if __name__ == "__main__":
     if len( sys.argv) != 2:
         print "usage: %s test_dir" % sys.argv[0]
         exit(1)
 
-
     #read config file
-    config = load_config( "%s/wrstat.config" % sys.argv[1])
+    test_dir = sys.argv[1]
+    config = load_config( "%s/wrstat.config" % test_dir)
     intervall = float( config[ "intervall"])
     modules = load_modules( config[ "modules"])
 
-    test_dir = sys.argv[1]
-
     print "sampling deamon: Making %f samples per second" % (1.0/intervall)
+    print "starting sampling deamon, use CTRL+C or SIGTERM to end"
 
+    #initialize signalhandler
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
-    print "starting sampling deamon, use CTRL+C or SIGTERM to end"
-
-    #starting capture
+    #initialize modules by presampling method
     for modname, module in modules.iteritems():
             print "%s: initialize %s" % ( __file__, modname)
             module.presampling( test_dir)
 
+    #signals wrstat to start the test command
     open( "%s/sampling-deamon.ready" % sys.argv[1], "w").close()
-    capture()
+
+    #create sampling thread for each module
+    cv = threading.Condition()
+    for modname, module in modules.iteritems():
+        thread = threading.Thread( target=sample, args=( module,))
+        thread.start()
+        sampling_threads.append( thread)
+
+    timer( intervall)
